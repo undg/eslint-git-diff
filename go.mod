1ad313464cea993732e4f7b724408281888cd8f1
undg <und3rdg@gmail.com>
2023-09-29 04:00:34 +0100

    better code organisation, colorful output

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git-diff-files
 
 go 1.21.1
+
+require github.com/creack/pty v1.1.18 // indirect
--- main.go
+++ main.go
@@ -3,80 +3,103 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/creack/pty"
 )
 
-func must(out []byte, err error) {
+type Flg struct {
+	fix      bool
+	eslint   bool
+	noEslint bool
+	branch   string
+	fetch    bool
+}
+
+func main() {
+	var files string
+	var command []string
+
+	var flg = Flg{}
+
+	{
+
+		flag.BoolVar(&flg.fix, "fix", false, "add --fix flag to eslint")
+		flag.BoolVar(&flg.fix, "f", false, "add --fix flag to eslint")
+
+		flag.BoolVar(&flg.eslint, "eslint", true, "run eslint with listed files")
+		flag.BoolVar(&flg.eslint, "e", true, "run eslint with listed files")
+
+		flag.BoolVar(&flg.noEslint, "no-eslint", false, "dont run eslint, only list files")
+
+		flag.StringVar(&flg.branch, "branch", "origin/dev", "branch to check files against")
+		flag.StringVar(&flg.branch, "b", "origin/dev", "branch to check files against")
+
+		flag.BoolVar(&flg.fetch, "fetch", false, "Before run git fetch")
+
+		flag.Parse()
+	}
+
+	if flg.fetch {
+		runGitFetch()
+	}
+
+	files = getFiles(flg)
+
+	if flg.noEslint {
+		flg.eslint = false
+	}
+
+	if flg.eslint {
+		runEslint(command, files, flg)
+	} else {
+		fmt.Println("\n" + files)
+	}
+
+}
+
+func getFiles(flg Flg) string {
+	gitArguments := strings.Fields("diff --name-only --diff-filter=d " + flg.branch)
+
+	out, err := exec.Command("git", gitArguments...).CombinedOutput()
+
+	if err != nil {
+		fmt.Printf("ERROR!\n%s\n", string(out))
+		panic(err)
+	}
+
+	return string(out)
+}
+
+func runGitFetch() {
+	out, err := exec.Command("git", "fetch").CombinedOutput()
+
 	if err != nil {
 		fmt.Printf("ERROR!\n%s\n", string(out))
 		panic(err)
 	}
 }
 
-func main() {
-	var flgFix bool
-	var flgEslint bool
-	var flgBranch string
-	var flgFetch bool
-
-    var files []string
-	var command []string
-
-	{
-		flag.BoolVar(&flgFix, "fix", false, "add --fix flag to eslint")
-		flag.BoolVar(&flgFix, "f", false, "add --fix flag to eslint")
-
-		flag.BoolVar(&flgEslint, "eslint", true, "run eslint with listed files")
-		flag.BoolVar(&flgEslint, "e", true, "run eslint with listed files")
-
-		flag.StringVar(&flgBranch, "branch", "origin/dev", "branch to check files against")
-		flag.StringVar(&flgBranch, "b", "origin/dev", "branch to check files against")
-
-		flag.BoolVar(&flgFetch, "fetch", false, "Before run git fetch")
-
-		flag.Parse()
+func runEslint(command []string, files string, flg Flg) {
+	command = append(command, "eslint_d")
+	if flg.fix {
+		command = append(command, "--fix")
 	}
 
-	// git fetch
-	{
-		if flgFetch {
-			out, err := exec.Command("git", "statu").CombinedOutput()
-			must(out, err)
-			fmt.Printf("output of git fetch:\n%s\n", string(out))
-		}
+	// run finall command
+	commands := command[0]
+	args := append(command[1:], strings.Fields(files)...)
+	cmd := exec.Command(commands, args...)
+
+	f, err := pty.Start(cmd)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-    // eslint
-	{
-		if flgEslint {
-			command = append(command, "eslint_d")
-			if flgFix {
-				command = append(command, "--fix")
-			}
-		} else {
-			command = append(command, "echo")
-		}
-	}
-
-    // get files
-    {
-        gitArguments := strings.Fields("diff --name-only --diff-filter=d " + flgBranch)
-
-        out, err := exec.Command("git", gitArguments...).CombinedOutput()
-        must(out, err)
-        fmt.Println(string(out))
-        files = strings.Fields(string(out))
-    }
-
-    // run finall command
-    {
-        cmd := command[0]
-        args := append(command[1:], files...)
-        out, err := exec.Command(cmd, args...).CombinedOutput()
-
-        must(out, err)
-        
-        fmt.Println(string(out))
-    }
+	io.Copy(os.Stdout, f)
 }
