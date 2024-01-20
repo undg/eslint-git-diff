3bf3fa4960a9df9f28449b34bf3de02737bc7ae9
undg <und3rdg@gmail.com>
2024-01-20 03:27:42 +0000

    add yellow, abstract command

--- test-watch.sh
+++ test-watch.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
+run_test() {
+		go test -v ./... | \
+			sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | \
+			sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | \
+			sed ''/RUN/s//$(printf "\033[33mRUN\033[0m")/''
+}
+
 echo "[$(date +%Y-%m-%d_%H-%m_%S)]"
 echo "========================================"
-go test ./...
+
+run_test
 
 echo ''
 
@@ -10,7 +18,7 @@ while true; do
 	inotifywait -qq -r -e create,close_write,modify,move,delete ./ &&
 		echo "[$(date +%Y-%m-%d_%H-%m_%S)]" &&
 		echo "========================================" &&
-		go test ./...
+		run_test
 
 	echo ''
 done
